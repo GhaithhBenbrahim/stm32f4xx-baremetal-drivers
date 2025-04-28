daa6407786ad983d4d92e6f5bbdec715a28ff46c
GhaithhBenbrahim <ghaythbenbrahim10@gmail.com>
2025-04-28 15:41:57 +0100

    i2c master send data

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/008spi_tx_it.c \
+../Src/008i2c_tx_master.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/008spi_tx_it.o \
+./Src/008i2c_tx_master.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/008spi_tx_it.d \
+./Src/008i2c_tx_master.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/008spi_tx_it.cyclo ./Src/008spi_tx_it.d ./Src/008spi_tx_it.o ./Src/008spi_tx_it.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/008i2c_tx_master.cyclo ./Src/008i2c_tx_master.d ./Src/008i2c_tx_master.o ./Src/008i2c_tx_master.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
