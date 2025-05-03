b98004a5db738134b3c8762905d8e1155aa030fc
GhaithhBenbrahim <ghaythbenbrahim10@gmail.com>
2025-05-04 00:26:19 +0100

    i2c interrupt exercice

--- Debug/Src/subdir.mk
+++ Debug/Src/subdir.mk
@@ -5,17 +5,17 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-../Src/008i2c_tx_master.c \
+../Src/010i2c_master_rx_IT.c \
 ../Src/syscalls.c \
 ../Src/sysmem.c 
 
 OBJS += \
-./Src/008i2c_tx_master.o \
+./Src/010i2c_master_rx_IT.o \
 ./Src/syscalls.o \
 ./Src/sysmem.o 
 
 C_DEPS += \
-./Src/008i2c_tx_master.d \
+./Src/010i2c_master_rx_IT.d \
 ./Src/syscalls.d \
 ./Src/sysmem.d 
 
@@ -27,7 +27,7 @@ Src/%.o Src/%.su Src/%.cyclo: ../Src/%.c Src/subdir.mk
 clean: clean-Src
 
 clean-Src:
-	-$(RM) ./Src/008i2c_tx_master.cyclo ./Src/008i2c_tx_master.d ./Src/008i2c_tx_master.o ./Src/008i2c_tx_master.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
+	-$(RM) ./Src/010i2c_master_rx_IT.cyclo ./Src/010i2c_master_rx_IT.d ./Src/010i2c_master_rx_IT.o ./Src/010i2c_master_rx_IT.su ./Src/syscalls.cyclo ./Src/syscalls.d ./Src/syscalls.o ./Src/syscalls.su ./Src/sysmem.cyclo ./Src/sysmem.d ./Src/sysmem.o ./Src/sysmem.su
 
 .PHONY: clean-Src
 
